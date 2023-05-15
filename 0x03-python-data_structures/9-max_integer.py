7967fa75404568bf24a4fba99b3f7ec12d10d6ef
raudaholamide <raudaholamide@gmail.com>
2023-05-15 14:53:53 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    max_val = my_list[0]
+    for i in my_list[1:]:
+        if max_val < i:
+            max_val = i
+    return max_val
