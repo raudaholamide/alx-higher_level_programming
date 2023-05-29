86aa138ed7f691bfc914d8a67938e2aa761735b6
raudaholamide <raudaholamide@gmail.com>
2023-05-29 10:06:48 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-
-def safe_print_list(list, x):
-    count = 0
-    res = []
-    for i in list[ : x]:
-        try:
-            res.append(i)
-        except TypeError:
-            print('Cannot perform operation on different types', arg)
-        else:
-            count+=1
-    
-
-    print(*res, sep = "")
-    return count
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    try:
+        for elements in my_list[0:x]:
+            print("{}".format(elements), end="")
+            counter += 1
+    except IndexError:
+        print("INVALID INDEX")
+    finally:
+        print()
+        return counter
