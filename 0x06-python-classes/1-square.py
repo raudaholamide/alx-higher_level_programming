7d8a3b8cff90f11f703a4f31358efdf93b1f4745
raudaholamide <123153259+raudaholamide@users.noreply.github.com>
2023-05-30 21:54:25 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-# A class Square that defines a square
+"""Define a class Square."""
 
 
 class Square:
-    def __init__(self, size):  # init allow square class to be used
-        self.__size = size   # asign private instance attribute size
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
