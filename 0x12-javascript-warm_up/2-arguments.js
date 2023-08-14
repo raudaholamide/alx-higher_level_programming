f4363695000ab71987e2239a8e5024a5b55ddb3f
raudaholamide <123153259+raudaholamide@users.noreply.github.com>
2023-08-15 00:20:11 +0100

    Create 2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arrArgs = process.argv;
+const len = arrArgs.length;
+if (len === 2) {
+  console.log('No argument');
+} else if (len === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
